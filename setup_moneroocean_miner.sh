ef9ff04d5cef0dedec0eb466c59b7f87b5609258
Leo Aqua <88901298+Leo-Aqua@users.noreply.github.com>
2024-01-05 20:47:06 +0100

    Update setup_moneroocean_miner.sh

--- setup_moneroocean_miner.sh
+++ setup_moneroocean_miner.sh
@@ -227,7 +227,9 @@ sed -i 's#"log-file": *null,#"log-file": "'$HOME/moneroocean/xmrig.log'",#' $HOM
 sed -i 's/"syslog": *[^,]*,/"syslog": true,/' $HOME/moneroocean/config.json
 
 cp $HOME/moneroocean/config.json $HOME/moneroocean/config_background.json
-sed -i 's/"background": *false,/"background": true,/' $HOME/moneroocean/config_background.json
+sed -i 's/\("background":\) *false,/\1 true,; s/\("pause-on-active":\) *false/\1 true/' $HOME/moneroocean/config_background.json
+
+
 
 # preparing script
 
